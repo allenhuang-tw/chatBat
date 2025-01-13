2910891d3accfd9993533ef23432ad54ef92bc86
allenhuang-tw <spy1k387j6@gmail.com>
2025-01-14 01:33:15 +0800

    Initial commit

---
+++ src/main/java/com/chatBat/ChatBatApplication.java
@@ -0,0 +1,13 @@
+package com.chatBat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatBatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatBatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chatBat/ChatBatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatBat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatBatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
